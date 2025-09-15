0245c7e8d2ff65e5738f2815a1404dc05c4abc13
onlymac <nirbhaygoswami789@gmail.com>
2025-09-15 23:21:40 +0530

    learn variables

---
+++ 01_basics/variable.js
@@ -0,0 +1,12 @@
+const accId=1234 
+let accName="ng"
+var accType="savings"
+address="Guj"
+
+//const variable cannet be  reassigned. it is final value non changed value
+
+// var is function scoped not block scoped it should not be used
+
+console.log(accId);//this is used to print value in console
+
+console.table([accId,accName,accType,address]);//this is used to print all values in table format
