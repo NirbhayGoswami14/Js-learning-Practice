9df284c6a390055523cc1d2d53b8163fe7c144cb
onlymac <nirbhaygoswami789@gmail.com>
2025-09-16 22:24:49 +0530

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,20 @@
+let num=20
+let str="hello"
+let bool=true
+let und=undefined
+let f=20.5
+
+/*
+data types 
+
+ num=>integer/bigint/number
+ str=>string
+ bool=>boolean
+ und=>undefined
+ f=>float
+ null
+ onbject
+*/
+
+// to find the data type of variable 
+console.log(typeof num)
