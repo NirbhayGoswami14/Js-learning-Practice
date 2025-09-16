0101902f1a671960592fa4c760b956d90a2115f1
onlymac <nirbhaygoswami789@gmail.com>
2025-09-16 23:30:15 +0530

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,27 @@
+let num=2
+let num1=num+1
+
+console.log(num1)
+
+/* ******** operations **********
+let num=2+2
+let num=2-2
+let num=2*2
+let num=2/2
+let num=2**5 this power operation
+let num=2%2
+
+these are basic operations
+*/
+
+let n3=1+"2"// output 12
+let n4="2"+1 // output 21
+let n=2+2+"2" // output 42
+let n2="2"+2+2 // output 222
+
+// there are some rules and guidline to write operations in js
+
+console.log(n)
+console.log(n2)
+console.log(n3)
+console.log(n4)
